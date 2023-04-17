bf9798d1dd7a97d4f14910ea39a709ca1e5c4c51
User <you@example.com>
2023-04-17 17:57:20 +0700

    Copy from classbook

--- Main.py
+++ Main.py
@@ -28,6 +28,50 @@ class Button(Rectangle):
         else:
             return False
 
+class InputBox:
+    def __init__(self, x, y, w, h, text=''):
+        self.rect = pg.Rect(x, y, w, h)
+        self.color = COLOR_INACTIVE
+        self.text = text
+        self.txt_surface = FONT.render(self.text, True, self.color)
+        self.active = False
+
+    def handle_event(self, event):
+        
+        if event.type == pg.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                self.active = not self.active
+            else:
+                self.active = False
+                
+            if self.active:
+                self.color = COLOR_ACTIVE 
+            else:
+                self.color = COLOR_INACTIVE
+            
+        if event.type == pg.KEYDOWN:
+            if self.active:
+                if event.key == pg.K_RETURN:
+                    print(self.text)
+                    self.text = ''
+                elif event.key == pg.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                else:
+                    self.text += event.unicode
+                # Re-render the text.
+                self.txt_surface = FONT.render(self.text, True, self.color)
+
+    def update(self):
+        # Resize the box if the text is too long.
+        width = max(200, self.txt_surface.get_width()+10)
+        self.rect.w = width
+
+    def draw(self, Screen):
+        # Blit the text.
+        Screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
+        # Blit the rect.
+        pg.draw.rect(Screen, self.color, self.rect, 2)
+
 import sys 
 import pygame as pg
 
@@ -35,28 +79,28 @@ pg.init()
 run = True
 win_x, win_y = 800, 480
 screen = pg.display.set_mode((win_x, win_y))
-obj = Rectangle(350,190,100,100,120,20,220)
+
+COLOR_INACTIVE = pg.Color('lightskyblue3') 
+COLOR_ACTIVE = pg.Color('dodgerblue2')
+FONT = pg.font.Font(None, 32)
+
+input_box1 = InputBox(100, 100, 140, 32)
+input_box2 = InputBox(100, 300, 140, 32)
+input_boxes = [input_box1, input_box2]
 
 while(run):
     screen.fill((255, 255, 255))
-        
-    obj.draw(screen)
-    
-    pg.display.update()
-    
-    for event in pg.event.get():
-        if event.type == pg.KEYDOWN and event.key == pg.K_a:
-            obj.x -= 10
-        
-        if event.type == pg.KEYDOWN and event.key == pg.K_d:
-            obj.x += 10
-        
-        if event.type == pg.KEYDOWN and event.key == pg.K_w:
-            obj.y -= 10
 
-        if event.type == pg.KEYDOWN and event.key == pg.K_s:
-            obj.y += 10
-            
+    for box in input_boxes:
+        box.update() 
+        box.draw(screen) 
+        
+    for event in pg.event.get():
+        for box in input_boxes:
+            box.handle_event(event)
         if event.type == pg.QUIT:
             pg.quit()
             run = False
+
+    pg.time.delay(1)
+    pg.display.update()
